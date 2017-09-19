c98e0e6646a4afb4f9cd9423cef42986b2f03580
Aislinn Russell <aislinnr@glazingvision.co.uk>
2017-09-19 13:59:45 +0100

    adding up to date database connection

--- Connect.php
+++ Connect.php
@@ -1,13 +1,13 @@
 <?php
         //database connection functions
 		$db_hostname = 'localhost';
-		$db_username = 'root';
-		$db_password = 'usbw';
+		$db_username = 'aislinnr';
+		$db_password = 'sQae379!';
 		$dbname = 'snysbarchive';
-				
+
 		$conn = mysql_connect($db_hostname, $db_username,$db_password); //connect to mySQL db
 		if(!$conn) die("Unable to connect to MySQL: " . mysql_error());  //error message if connection unsuccessful
 
-		mysql_select_db($dbname) or die("Unable to select database: " . mysql_error());//select database 
+		mysql_select_db($dbname) or die("Unable to select database: " . mysql_error());//select database
 
 	?>
